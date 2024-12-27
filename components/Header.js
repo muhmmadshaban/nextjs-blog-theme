a56a3cc09ad106e8ff60507edc113cde9aea4e3d
Muhmmad Shaban <97331866+muhmmadshaban@users.noreply.github.com>
2024-12-27 11:50:07 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,17 @@
 import Link from 'next/link';
+import Image from 'next/image'; // Import the Image component from Next.js
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="block w-12 h-12 mx-auto mb-4 rounded-full bg-gradient-conic from-gradient-3 to-gradient-4" />
+      {/* Use the Image component for optimized image loading */}
+      <Image 
+        src="s.png" // Path to your image
+        alt={`${name} Logo`} 
+        width={48} // Set the width of the image
+        height={48} // Set the height of the image
+        className="block mx-auto mb-4 rounded-full" // Add any additional classes you need
+      />
       <p className="text-2xl text-center dark:text-white">
         <Link href="/">
           {name}
